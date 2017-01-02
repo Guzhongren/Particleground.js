06277465514f99d2289bfa1de05b346f5d170c2e
barrior <barrior@qq.com>
2017-01-02 21:20:06 +0800

    test

--- test/particleground.spec.js
+++ test/particleground.spec.js
@@ -1,20 +1,20 @@
 const chai = require('chai');
 const jsdom = require('mocha-jsdom');
 
-const expect = chai.expect;
+const assert = chai.assert;
 
 //const pjs = require('../dev/particleground.all');
 
 function limitRandom(max, min) {
     return max === min ? max : (Math.random() * (max - min) + min);
 }
-/*
+
 describe('limitRandom()', () => {
-    console.log(jsdom());
     it('limitRandom(20, 2) 应该输出 2 ~ 20 之间的数字', () => {
-        return 51 <= limitRandom(20, 2) < 200
+        const num = limitRandom(20, 2);
+        assert.equal(true, 2 <= num && num < 20 ? true : false);
     });
-});*/
+});
 
 describe('util.pInt()', () => {
 
@@ -26,6 +26,6 @@ describe('util.pInt()', () => {
     });
 
     it(`util.pInt('520px') 应该输出 520`, () => {
-        expect(Particleground.util.pInt('520px')).to.equal(520);
+        assert.equal(Particleground.util.pInt('520px'), 520);
     });
 });
